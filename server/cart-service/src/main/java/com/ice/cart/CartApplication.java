4637a8c0101c9a5987574492944c5a8c116204be
ganbin <jlau.ice@gmail.com>
2025-12-14 17:23:56 +0800

    feat(cart): Configures the default Feign log level to FULL.

--- server/cart-service/src/main/java/com/ice/cart/CartApplication.java
+++ server/cart-service/src/main/java/com/ice/cart/CartApplication.java
@@ -1,11 +1,12 @@
 package com.ice.cart;
 
+import com.ice.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "com.ice.api.client")
+@EnableFeignClients(basePackages = "com.ice.api.client", defaultConfiguration = DefaultFeignConfig.class)
 @MapperScan("com.ice.cart.mapper")
 @SpringBootApplication
 public class CartApplication {

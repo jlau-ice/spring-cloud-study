a2870c6a1143441be9216f37a2f87084d7d2f071
ganbin <jlau.ice@gmail.com>
2025-12-14 13:33:39 +0800

    docs(openapi): Update API documentation configuration information

--- server/cart-service/src/main/java/com/ice/cart/CartApplication.java
+++ server/cart-service/src/main/java/com/ice/cart/CartApplication.java
@@ -1,9 +1,11 @@
 package com.ice.cart;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ice.cart.mapper")
 public class CartApplication {
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);

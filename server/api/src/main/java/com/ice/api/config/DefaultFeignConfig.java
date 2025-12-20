c1055bc1b66ba993075799b86791950c28c8b3f8
ganbin <jlau.ice@gmail.com>
2025-12-20 17:22:43 +0800

    feat(api): Adds a Feign request interceptor to pass user information.

--- server/api/src/main/java/com/ice/api/config/DefaultFeignConfig.java
+++ server/api/src/main/java/com/ice/api/config/DefaultFeignConfig.java
@@ -1,11 +1,27 @@
 package com.ice.api.config;
 
+import com.ice.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLogLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor() {
+        return template -> {
+            // 获取登录用户
+            Long userId = UserContext.getUser();
+            if (userId == null) {
+                // 如果为空则直接跳过
+                return;
+            }
+            // 如果不为空则放入请求头中，传递给下游微服务
+            template.header("user-info", userId.toString());
+        };
     }
 }

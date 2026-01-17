3cf2d3163b8be19788d003c6b0a377b0a32ff95c
ice <jlau.ice@gmail.com>
2026-01-17 13:32:05 +0800

    refactor(config): 将微服务配置迁移到Nacos并优化结构

--- server/user-service/src/main/java/com/ice/user/config/SecurityConfig.java
+++ server/user-service/src/main/java/com/ice/user/config/SecurityConfig.java
@@ -30,4 +30,24 @@ public class SecurityConfig {
                 properties.getAlias(),
                 properties.getPassword().toCharArray());
     }
+    public int trailingZeroes(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+        }
+        String a = String.valueOf(res);
+        char[] arr = a.toCharArray();
+        int resu = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == '0') {
+                resu++;
+            } else {
+                break;
+            }
+        }
+        return resu;
+    }
 }
